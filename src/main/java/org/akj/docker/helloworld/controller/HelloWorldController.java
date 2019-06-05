af728f7e6e952903a0267a9d72810bd3830bf4b2
Jamie Zhang <akjamie.zhang@gmail.com>
2019-06-05 10:33:52 +0800

    change git ignore

--- src/main/java/org/akj/docker/helloworld/controller/HelloWorldController.java
+++ src/main/java/org/akj/docker/helloworld/controller/HelloWorldController.java
@@ -1,16 +1,26 @@
 package org.akj.docker.helloworld.controller;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("helloworld")
 public class HelloWorldController {
 
 	@RequestMapping(method = RequestMethod.GET, value = "/")
-	public String sayhi() {
+	public String sayhi(HttpServletRequest request) throws UnknownHostException {
 
-		return "Hello Docker World";
+		InetAddress ip = InetAddress.getLocalHost();
+
+		return "[Succ]Hello Docker World, request rec'd from [Hostname]:" + ip.getCanonicalHostName()
+				+ ",\r\n[RemoteAddr]:" + request.getRemoteAddr() + ",\r\n[x-forwarded-for]:"
+				+ request.getHeader("x-forwarded-for") + ",\r\n[x-real-ip]:" + request.getHeader("x-real-ip");
 	}
 
 }
--- src/test/java/org/akj/docker/helloworld/controller/HelloWorldControllerTest.java
+++ src/test/java/org/akj/docker/helloworld/controller/HelloWorldControllerTest.java
@@ -1,14 +1,5 @@
 package org.akj.docker.helloworld.controller;
 
-import org.junit.Assert;
-import org.junit.Test;
-
-
 public class HelloWorldControllerTest {
-
-	@Test
-	public void test() {
-		Assert.assertTrue(true);
-	}
 
 }
